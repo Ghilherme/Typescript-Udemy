d8b119ccd61053af93f508a48cc90f92149ad0af
Guilherme <g.p.oliveira@hotmail.com>
2017-10-11 19:01:05 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+//Funcao tipada
+var ola = function (nome, sobrenome) {
+    console.log("olá " + nome + " " + sobrenome);
+};
+ola("Gui", "Pereira");
+//Tipagem variaveis
+var mensagem = "TEste";
+var numeros = 10;
+var booleano = true;
+var listastring = ["x1", "x2", "x3"];
+var listanumero = [1, 2, 3];
